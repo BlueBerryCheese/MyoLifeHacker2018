9ca241f562b310a6a51773af91ac972324006f04
Reussite <sho6368@naver.com>
2018-04-09 02:12:01 +0900

    Tab_fragment 에 Bluetooth연동 및 SystemFeature부분 구현 예상으론 이제 새로운 detectMethod를 구현해서 하나 붙이는게 편할것으로 예상된다.(현재는 안되는 상태)

--- app/src/main/java/blueberrycheese/myolifehacker/TabFragment1.java
+++ app/src/main/java/blueberrycheese/myolifehacker/TabFragment1.java
@@ -1,8 +1,12 @@
 package blueberrycheese.myolifehacker;
 
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -12,6 +16,15 @@ import android.view.ViewGroup;
 
 import com.imangazaliev.circlemenu.CircleMenu;
 import com.imangazaliev.circlemenu.CircleMenuButton;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.HashMap;
+
+import blueberrycheese.myolifehacker.myo_manage.GestureSaveMethod;
+import blueberrycheese.myolifehacker.myo_manage.MyoGattCallback;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,14 +37,18 @@ import com.imangazaliev.circlemenu.CircleMenuButton;
 public class TabFragment1 extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String TAG = "TabFragment1";
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private View view;
+    private CircleMenu circleMenu;
+    private CircleMenuButton circleMenuButton_volume ;
     private OnFragmentInteractionListener mListener;
+    private GestureSaveMethod saveMethod;
 
     public TabFragment1() {
         // Required empty public constructor
@@ -70,16 +87,25 @@ public class TabFragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
-
+        view = inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
+        mHandler = new Handler();
         //Main CirecleMenu 관련
-        CircleMenu circleMenu = (CircleMenu) view.findViewById(R.id.circleMenu);
+        circleMenu = (CircleMenu) view.findViewById(R.id.circleMenu);
+        circleMenuButton_volume = (CircleMenuButton)view.findViewById(R.id.volume);
         circleMenu.setOnItemClickListener(new CircleMenu.OnItemClickListener() {
             @Override
             public void onItemClick(CircleMenuButton menuButton) {
 
             }
         });
+
+        circleMenuButton_volume.setOnClickListener(new CircleMenuButton.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
 
         circleMenu.setEventListener(new CircleMenu.EventListener() {
             @Override
@@ -143,6 +169,22 @@ public class TabFragment1 extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onStop() {
+        try{
+            EventBus.getDefault().unregister(this);         //이벤트버스는 시행되면 계속 그곳에서 이벤트가 발생하는데 문제가 일어날수있다 생각하여 멈추거나할때 이벤트를 꺼주는것을 해야함 아니면 베터리소모가 크답니다.
+        }catch (Exception e){}
+        super.onStop();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        try {
+            EventBus.getDefault().register(this);           //이벤트 버스 다시 키는 역활
+        }catch (Exception e){}
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -156,5 +198,32 @@ public class TabFragment1 extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+    BluetoothDevice device;
+    MyoGattCallback mMyoCallback;
+    BluetoothGatt mBluetoothGatt;
+    Handler mHandler;
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void testEvent(EventData event){
+        Log.e("test_event", event.device.getName() + "connected !!");
+        HashMap<String,View> views = new HashMap<String,View>();
+
+        device = event.device;
+        mMyoCallback = new MyoGattCallback(mHandler, null, views,null,-1);  //이곳에서 문제가 일어나서 현재 이페이지에서밖에 시행이 안됨 inds_num(제스처 몇번을 저장할 것인가에 대한 내용이 담겨져 있음)
+        mBluetoothGatt = device.connectGatt(getContext(), false, mMyoCallback);
+        mMyoCallback.setBluetoothGatt(mBluetoothGatt);
+
+        if (mBluetoothGatt == null) {
+            Log.d(TAG,"False EMG");
+        } else {
+            saveMethod  = new GestureSaveMethod(-1,view.getContext());
+            if (saveMethod.getSaveState() == GestureSaveMethod.SaveState.Have_Saved) {
+                Log.e("test_event", event.device.getName() + "connected !!& Linking gesture");
+                //gestureText.setText("DETECT Ready");
+            } else {
+                //gestureText.setText("Teach me \'Gesture\'");
+            }
+        }
     }
 }
